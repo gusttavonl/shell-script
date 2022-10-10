86ad94cb40012b26b05acc5b32b6fe565b8fe131
@gustavo.lima <gustavo.lima@vitta.me>
2022-10-10 17:58:07 -0300

    feat: add echo variables tutorial

---
+++ variables.sh
@@ -0,0 +1,11 @@
+# Write a var with nameVar=value
+helloWorld="Hello World" 
+
+# Print var value with echo $nameVar
+echo $helloWorld
+
+# Write a var with nameVar=value
+number=1
+
+# Print var value with echo $nameVar
+echo $number
