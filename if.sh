3b775ffec84f49a819f79a23912d365b5913921c
GustavoNoronha0 <admin@gustavonoronha.com.br>
2022-10-17 16:33:22 -0300

    feat: add if structure example

---
+++ if.sh
@@ -0,0 +1,13 @@
+
+# create a new var
+hour=9
+
+# init if structure to verify if hour is equal at nine
+if  [ hour=9 ]; then
+ # if hour is equal at nine return this
+ echo "now is morning"
+ # if hour is not equal at nine return this
+ else
+   echo "now is not morning"
+# finish if
+fi
